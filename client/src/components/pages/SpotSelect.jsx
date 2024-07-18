b2f7dbcd833db2dc138d1657dfa189d2cf65bc6e
이지훈 <155136709+dlwlgnsrhy@users.noreply.github.com>
2024-07-19 08:56:40 +0900

    add front-end

---
+++ client/src/components/pages/SpotSelect.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SpotSelect=()=> {
+    
+        return (
+            <div>
+                    <h1>Welcome to SpotPage</h1>
+                <video autoPlay loop muted className="video-background">
+                <source src="/videos/surfing.mp4" type="video/mp4" />
+            </video>
+            </div>
+        );
+    }
+
+export default SpotSelect;
