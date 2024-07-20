dab5fafcde2d70a07d95d0bcb1bc7120b7b581eb
이지훈 <155136709+dlwlgnsrhy@users.noreply.github.com>
2024-07-20 12:54:07 +0900

    add axios, external api

--- server/middlewares/cacheMiddleware.js
+++ server/middlewares/cacheMiddleware.js
@@ -1,28 +1,26 @@
 const redisClient = require('../config/redisConfig');
+const axios = require('axios');
 
-const cacheMiddleware = (req, res, next) => {
+
+const cacheMiddleware = async (req, res, next) => {
     const { url } = req;
-
-    redisClient.get(url, (err, data) => {
-        if (err) {
-            console.error('Redis error:', err);
-            next();
-            return;
-        }
-
+    try {
+        const data = await redisClient.get(url);
         if (data) {
             res.send(JSON.parse(data));
-            return;
+        } else {
+            // 오리지널 send 메서드를 백업
+            res.sendResponse = res.send;
+            res.send = async (body) => {
+                await redisClient.set(url, JSON.stringify(body), 'EX', 3600); // 1시간 동안 캐싱
+                res.sendResponse(body);
+            };
+            next();
         }
-
-        res.sendResponse = res.send;
-        res.send = (body) => {
-            redisClient.set(url, JSON.stringify(body), 'EX', 3600); // 1시간 동안 캐싱
-            res.sendResponse(body);
-        };
-
+    } catch (err) {
+        console.error('Redis error:', err);
         next();
-    });
+    }
 };
 
 module.exports = cacheMiddleware;
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,14 +1,36 @@
 const express =require('express');
 const router =express.Router();
+const cacheMiddleware= require('../middlewares/cacheMiddleware');
+const axios =require('axios');
 
-//캐싱이 적용될 경로
-router.get('/some-data',(req,res)=>{
-    const responseData ={
-        message : 'This data is cached for 1 hour',
-        timestamp : new Data()
-    };
-    res.send(responseData);
+// 외부 API 호출 함수
+const fetchExternalAPI = async (url) => {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw new Error('Error fetching data from external API');
+    }
+};
 
+// 날씨 API 경로
+router.get('/weather', cacheMiddleware, async (req, res) => {
+    try {
+        const weatherData = await fetchExternalAPI('https://api.example.com/weather');
+        res.send(weatherData);
+    } catch (error) {
+        res.status(500).send('Error fetching weather data');
+    }
+});
+
+// 뉴스 API 경로
+router.get('/news', cacheMiddleware, async (req, res) => {
+    try {
+        const newsData = await fetchExternalAPI('https://api.example.com/news');
+        res.send(newsData);
+    } catch (error) {
+        res.status(500).send('Error fetching news data');
+    }
 });
 
 module.exports=router;
