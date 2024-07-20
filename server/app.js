903f3704a7b03bc59e1535694bb582239ae42753
이지훈 <155136709+dlwlgnsrhy@users.noreply.github.com>
2024-07-20 13:25:20 +0900

    add swagger-ui,nodemon
    server측 실행 명령어
    개발시 npm run dev
    프로덕션시 npm start
    ---
    swagger-ui
    http://localhost:5000/api-docs/

--- server/app.js
+++ server/app.js
@@ -4,8 +4,30 @@ const sessionConfig = require('./config/sessionConfig');
 const authRoutes = require('./routes/authRoutes');
 const cacheMiddleware =require('./middlewares/cacheMiddleware');
 const apiRoutes =require('./routes/apiRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
 
 const app = express();
+
+const swaggerOption ={
+    swaggerDefinition:{
+        openapi:'3.0.0',
+        info:{
+            title: 'API Documentation',
+            version:'1.0.0',
+            description:'API Documentation for the application'
+        },
+        servers:[
+            {
+                url: 'http://localhost:5000',
+                description:'Development server'
+            }
+        ]
+    },
+    apis:['./routes/*.js'] //api경로 수정 부분
+};
+const swaggerDocs =swaggerJsdoc(swaggerOption);
 
 //Json 요청의 본문을 파싱하기 위해 Express의 기본 미들웨어를 사용
 app.use(express.json());
@@ -14,8 +36,8 @@ app.use(express.json());
 app.use(sessionConfig);
 
 
-app.use('/auth', authRoutes);
-app.use('api',apiRoutes); //특정 경로에 대해서만 캐싱 미들웨어를 적용
-
+app.use('/auth', authRoutes); //회원가입,로그인 로직 에서의 미들웨어를 적용
+app.use('/api',apiRoutes); //특정 경로에 대해서만 캐싱 미들웨어를 적용
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocs));  //swagger-ui
 
 module.exports = app;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,8 +3,88 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const cacheMiddleware =require('../middlewares/cacheMiddleware');
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: "john_doe"
+ *               password:
+ *                 type: string
+ *                 example: "your_password"
+ *               email:
+ *                 type: string
+ *                 example: "john.doe@example.com"
+ *               phone_number:
+ *                 type: string
+ *                 example: "123-456-7890"
+ *               preference:
+ *                 type: string
+ *                 example: "tech_blog"
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Username, password, and phone number are required
+ *       500:
+ *         description: Error registering user
+ */
 router.post('/register', authController.register);
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Log in a user
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: "john_doe"
+ *               password:
+ *                 type: string
+ *                 example: "your_password"
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *       401:
+ *         description: Unauthorized
+ */
 router.post('/login', authController.login);
+
+/**
+ * @swagger
+ * /auth/protected:
+ *   get:
+ *     summary: Access protected route
+ *     tags:
+ *       - Auth
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Access granted
+ *       401:
+ *         description: Unauthorized
+ */
 router.get('/protected', authController.authenticateSession, (req, res) => {
     res.send('This is a protected route');
 });
