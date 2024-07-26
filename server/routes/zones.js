be8ef878f7dc9ff2561014b99b58b54e6bc6482d
hyobin <tpids99@gmail.com>
2024-07-26 17:38:43 +0900

    add likes, commet, notifi

--- server/routes/zones.js
+++ server/routes/zones.js
@@ -1,13 +1,61 @@
 const express = require('express');
-const db = require('../config/dbConfig'); 
+const db = require('../config/dbConfig');
 
 const router = express.Router(); 
 
+/**
+ * @swagger
+ * tags:
+ *   name: Zones
+ *   description: API for managing surfing and diving zones
+ */
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Returns the API description
+ *     responses:
+ *       200:
+ *         description: Successful response
+ */
 router.get('/', (req, res) => {
     res.send('Surfing Zone and Diving Zone API');
 });
 
-// Surfing Zones Routes
+/**
+ * @swagger
+ * /zones/surfing-zones:
+ *   get:
+ *     summary: Get all surfing zones
+ *     tags: [Zones]
+ *     responses:
+ *       200:
+ *         description: A list of surfing zones
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   surfing_zone_id:
+ *                     type: integer
+ *                   name:
+ *                     type: string
+ *                   description:
+ *                     type: string
+ *                   latitude:
+ *                     type: number
+ *                   longitude:
+ *                     type: number
+ *                   difficulty_level:
+ *                     type: string
+ *                   average_wave_height:
+ *                     type: number
+ *                   nearby_facilities:
+ *                     type: string
+ */
 router.get('/surfing-zones', async (req, res) => {
     try {
         const [rows] = await db.query('SELECT * FROM Surfing_Zone');
@@ -17,6 +65,46 @@ router.get('/surfing-zones', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /zones/surfing-zones/{id}:
+ *   get:
+ *     summary: Get a specific surfing zone by ID
+ *     tags: [Zones]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID of the surfing zone
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A single surfing zone
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 surfing_zone_id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 description:
+ *                   type: string
+ *                 latitude:
+ *                   type: number
+ *                 longitude:
+ *                   type: number
+ *                 difficulty_level:
+ *                   type: string
+ *                 average_wave_height:
+ *                   type: number
+ *                 nearby_facilities:
+ *                   type: string
+ *       404:
+ *         description: Surfing zone not found
+ */
 router.get('/surfing-zones/:id', async (req, res) => {
     const { id } = req.params;
     try {
@@ -30,6 +118,46 @@ router.get('/surfing-zones/:id', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /zones/surfing-zones:
+ *   post:
+ *     summary: Create a new surfing zone
+ *     tags: [Zones]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               latitude:
+ *                 type: number
+ *               longitude:
+ *                 type: number
+ *               difficulty_level:
+ *                 type: string
+ *               average_wave_height:
+ *                 type: number
+ *               nearby_facilities:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 surfing_zone_id:
+ *                   type: integer
+ *       500:
+ *         description: Server error
+ */
 router.post('/surfing-zones', async (req, res) => {
     const { name, description, latitude, longitude, difficulty_level, average_wave_height, nearby_facilities } = req.body;
     try {
@@ -43,7 +171,39 @@ router.post('/surfing-zones', async (req, res) => {
     }
 });
 
-// Diving Zones Routes
+/**
+ * @swagger
+ * /zones/diving-zones:
+ *   get:
+ *     summary: Get all diving zones
+ *     tags: [Zones]
+ *     responses:
+ *       200:
+ *         description: A list of diving zones
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   diving_zone_id:
+ *                     type: integer
+ *                   name:
+ *                     type: string
+ *                   description:
+ *                     type: string
+ *                   latitude:
+ *                     type: number
+ *                   longitude:
+ *                     type: number
+ *                   difficulty_level:
+ *                     type: string
+ *                   average_wave_height:
+ *                     type: number
+ *                   nearby_facilities:
+ *                     type: string
+ */
 router.get('/diving-zones', async (req, res) => {
     try {
         const [rows] = await db.query('SELECT * FROM Diving_Zone');
@@ -53,6 +213,46 @@ router.get('/diving-zones', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /zones/diving-zones/{id}:
+ *   get:
+ *     summary: Get a specific diving zone by ID
+ *     tags: [Zones]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID of the diving zone
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A single diving zone
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 diving_zone_id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 description:
+ *                   type: string
+ *                 latitude:
+ *                   type: number
+ *                 longitude:
+ *                   type: number
+ *                 difficulty_level:
+ *                   type: string
+ *                 average_wave_height:
+ *                   type: number
+ *                 nearby_facilities:
+ *                   type: string
+ *       404:
+ *         description: Diving zone not found
+ */
 router.get('/diving-zones/:id', async (req, res) => {
     const { id } = req.params;
     try {
@@ -66,6 +266,46 @@ router.get('/diving-zones/:id', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /zones/diving-zones:
+ *   post:
+ *     summary: Create a new diving zone
+ *     tags: [Zones]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               latitude:
+ *                 type: number
+ *               longitude:
+ *                 type: number
+ *               difficulty_level:
+ *                 type: string
+ *               average_wave_height:
+ *                 type: number
+ *               nearby_facilities:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 diving_zone_id:
+ *                   type: integer
+ *       500:
+ *         description: Server error
+ */
 router.post('/diving-zones', async (req, res) => {
     const { name, description, latitude, longitude, difficulty_level, average_wave_height, nearby_facilities } = req.body;
     try {
